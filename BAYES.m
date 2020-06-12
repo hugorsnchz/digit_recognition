c1ff517f21b0dd9ae19cbfb8534b773790dcda05
Pablo-dopazo <64312579+Pablo-dopazo@users.noreply.github.com>
2020-06-12 11:57:31 +0200

    Add files via upload

--- BAYES.m
+++ BAYES.m
@@ -4,19 +4,25 @@ close all
 
 %% Bayesiano
 
-batch=9000;
-ncompca=200;
+batch=8000;
+ncompca=51;
 
 load('Trainnumbers.mat')
 
-% if exist('reducedData','var') == 0
-%     load('reducedData.mat')
-% end
-
 [Trainnumbers.image, porcentaje]=function_pca(Trainnumbers,ncompca);
 
-tipo='linear';
-%tipo='quadratic';
+%tipo='linear';
+tipo='quadratic';
 
 output=classify(Trainnumbers.image(:,batch+1:10000)',Trainnumbers.image(:,1:batch)',Trainnumbers.label(:,1:batch),tipo);
 acierto=1-length(find(output'~=Trainnumbers.label(:,batch+1:10000)))/length(Trainnumbers.label(:,batch+1:10000));
+
+cm = confusionmat(Trainnumbers.label(:,batch+1:10000),output');
+numeros=[0 1 2 3 4 5 6 7 8 9];
+
+cc=confusionchart(cm,numeros);
+
+cc.Title = 'Digit Classification Using Bayessian Classifier';
+cc.RowSummary = 'row-normalized';
+cc.ColumnSummary = 'column-normalized';
+
